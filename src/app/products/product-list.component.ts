c8a1e4550696f192d0daf608f2779a1bafb718d2
Ujjwal Gupta <ujjwal@quaqua.com>
2020-08-19 12:15:46 +0530

    Async Pipe - subscribe to observable when component initialized

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './products';
 import { ProductService } from './product.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -17,34 +18,36 @@ export class ProductListComponent implements OnInit{
    showImage: boolean = false;
    errorMessage: string;
 
-   _listFilter: string;
+   /**_listFilter: string;
    get listFilter(): string {
        return this._listFilter;
    }
    set listFilter(value: string){
        this._listFilter = value;
        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-   }
+   }**/
 
-   filteredProducts: IProduct[];
-   products: IProduct[] = [];
+   //filteredProducts: IProduct[];
+   products$: Observable<IProduct[]>;
 
    constructor(private productService: ProductService){}
 
    ngOnInit(): void {
-    this.productService.getProduct().subscribe(data => {
-        this.products = data;
-        this.filteredProducts = this.products;
-    },err => {
-        this.errorMessage = err
-    });   
+    // this.productService.getProduct().subscribe(data => {
+    //     this.products = data;
+    //     this.filteredProducts = this.products;
+    // },err => {
+    //     this.errorMessage = err
+    // });
+
+       this.products$ = this.productService.getProduct();
    }
 
-   performFilter(filterBy: string): IProduct[] {
+   /**performFilter(filterBy: string): IProduct[] {
        filterBy = filterBy.toLocaleLowerCase();
        return this.products.filter((product: IProduct) => 
           product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-   }
+   }**/
     
    toggleImage(): void{
        this.showImage = !this.showImage;
